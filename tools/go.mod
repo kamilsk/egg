849bbc36f915aaecf6ec3fcc0a63b14ab9604cff
Kamil Samigullin <kamil@samigullin.info>
2020-01-01 21:14:55 +0300

    optimize toolkit

--- tools/go.mod
+++ tools/go.mod
@@ -3,13 +3,8 @@ module github.com/kamilsk/egg/tools
 go 1.11
 
 require (
-	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/golang/mock v1.3.1
 	github.com/golangci/golangci-lint v1.22.2
-	github.com/itchyny/gojq v0.7.0
-	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
-	github.com/lestrrat-go/strftime v1.0.1 // indirect
-	github.com/tebeka/strftime v0.1.3 // indirect
 	golang.org/x/tools v0.2.2
 )
 
--- tools/tools.go
+++ tools/tools.go
@@ -7,11 +7,9 @@ package tools
 import (
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/itchyny/gojq/cmd/gojq"
 	_ "golang.org/x/tools/cmd/goimports"
 )
 
 //go:generate go build -v -o=${ROOT}bin/mockgen github.com/golang/mock/mockgen
 //go:generate go build -v -o=${ROOT}bin/golangci-lint github.com/golangci/golangci-lint/cmd/golangci-lint
-//go:generate go build -v -o=${ROOT}bin/gojq github.com/itchyny/gojq/cmd/gojq
 //go:generate go build -v -o=${ROOT}bin/goimports golang.org/x/tools/cmd/goimports
