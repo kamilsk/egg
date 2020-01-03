f8e5116fb94915c63202e99339deb701c071f7f5
Kamil Samigullin <kamil@samigullin.info>
2020-01-03 19:31:06 +0300

    add force flag for tools init and fix tools regen (panic)

--- internal/cmd/tools/init.go
+++ internal/cmd/tools/init.go
@@ -2,12 +2,17 @@ package tools
 
 import (
 	"context"
+	"path/filepath"
 
 	"github.com/izumin5210/gex"
+	"github.com/izumin5210/gex/pkg/tool"
 	"github.com/spf13/cobra"
 )
 
 func NewInitCommand(cfg *gex.Config) *cobra.Command {
+	var (
+		force bool
+	)
 	cmd := &cobra.Command{
 		Use: "init",
 		RunE: func(*cobra.Command, []string) error {
@@ -15,11 +20,17 @@ func NewInitCommand(cfg *gex.Config) *cobra.Command {
 			defer cancel()
 
 			repository, err := cfg.Create()
+			if force {
+				path := filepath.Join(cfg.RootDir, cfg.ManifestName)
+				m := tool.NewManifest(nil, cfg.ManagerType)
+				return tool.NewWriter(cfg.FS).Write(path, m)
+			}
 			if err != nil {
 				return err
 			}
 			return repository.Add(ctx)
 		},
 	}
+	cmd.Flags().BoolVar(&force, "force", false, "initialize from scratch")
 	return cmd
 }
